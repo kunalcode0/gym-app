395225bd16a829079b9db2e87325605c2020f126
kunal chauhan <kckr0115@gmail.com>
2022-09-04 21:08:44 -0700

    change in nav bar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React  from 'react'
 import './Navbar.css'
+import {useState} from 'react';
 import {GiMuscleUp} from 'react-icons/gi'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import {ImCross} from "react-icons/im"
 
 function Navbar() {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = event => {
+    // 👇️ toggle isActive state on click
+    setIsActive(current => !current);
+  };
  
   return (
   <section className="navbar" id="navbar">
@@ -13,21 +22,25 @@ function Navbar() {
    </div>
 
    <div className="nav__options">
-    <ul className="nav__links">
+    <ul className={isActive ? 'show__nav-links' : 'nav__links'}>
         <li className="nav__items">
-           <a href="#"> Home</a>
+           <a href="#"  onClick={handleClick}> Home</a>
         </li>
 
         <li className="nav__items">
-        <a href="#program"> Program</a>
+        <a href="#program" onClick={handleClick}> Program</a>
         </li>
 
         <li className="nav__items">
-        <a href="#choose"> Choose Us</a>
+        <a href="#choose" onClick={handleClick}> Choose Us</a>
         </li>
 
         <li className="nav__items">
-        <a href="#plans"> Plans</a>
+        <a href="#plans"  onClick={handleClick}> Plans</a>
+        </li>
+
+        <li className="nav__items">
+         <ImCross className='nav__icon3' onClick={handleClick}/>
         </li>
 
        <button className='nav__btn'> <a href="#contact">Contact Now</a></button>
@@ -35,7 +48,7 @@ function Navbar() {
 
   
 </div>
-<GiHamburgerMenu className='nav__icon2' />
+<GiHamburgerMenu className='nav__icon2'  onClick={handleClick}/>
   
 
   </section>
